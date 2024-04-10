0a9fbfca6c7ce6dc30839019c7cb6fb6ab3f600f
Kristian Quirapas <kmquirapas@gmail.com>
2024-04-10 15:08:37 +0800

    add: almost working just makign it async

--- bin/client.rs
+++ bin/client.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, client!");
+use anyhow::{Context, Result};
+use axum::http::Uri;
+use futures_util::{SinkExt, StreamExt};
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+const WEBSOCKETS_URI: &str = "ws://127.0.0.1:3000/ws";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let uri = Uri::from_static(WEBSOCKETS_URI);
+    let (mut socket, _) = ClientBuilder::from_uri(uri)
+        .connect()
+        .await
+        .with_context(|| "Failed to connect.")?;
+
+    let stdin = tokio::io::stdin();
+    let mut stdin = BufReader::new(stdin).lines();
+
+    loop {
+        let cli_input = stdin
+            .next_line()
+            .await
+            .with_context(|| "Failed to read from stdin.")?;
+
+        if let Some(msg) = cli_input {
+            tokio::spawn(async move { socket.send(Message::text(msg)).await });
+        }
+    }
 }
--- bin/server.rs
+++ bin/server.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, server!");
+use anyhow::{Context, Result};
+use axum::{
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+
+const LOCALHOST: &str = "127.0.0.1";
+const PORT: &str = "3000";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = tokio::net::TcpListener::bind(format!("{LOCALHOST}:{PORT}"))
+        .await
+        .with_context(|| "Failed to start TCP listener.")?;
+
+    let app = Router::new()
+        .route("/", get(|| async { "hotdog" }))
+        .route("/ws", get(handler));
+
+    axum::serve(listener, app)
+        .await
+        .with_context(|| "Failed to serve.")?;
+
+    Ok(())
+}
+
+async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            return;
+        };
+
+        // error converting Message to &str
+        if msg.to_text().is_err() {
+            return;
+        }
+
+        match msg.to_text() {
+            Ok(text) => println!("Sent from client: {text}"),
+            Err(_) => return,
+        }
+    }
 }
